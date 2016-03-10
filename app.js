c90575b1a2eb0c06f2d67fd4936dbf245a653026
Bill <bill.du@cleargrass.com>
2016-03-10 11:45:35 +0800

    创建db目录

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs')
 
 var sqlite = require('sqlite3')
 
@@ -24,39 +25,42 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-var db = new sqlite.Database('db/db.db', function() {
-  db.run("create table member(\
-    id integer PRIMARY KEY autoincrement\
-    , name varchar(15)\
-    , image_url varchar(1000)\
-    , intro varchar(100)\
-    , weibo_url varchar(100)\
-    , weibo_snippet varchar(1000)\
-    , position integer default 10\
-    , join_date integer\
-  )"
-    , function(error) {
-      if (!error) {
-        var insert = "INSERT INTO member(name, image_url, intro, weibo_url, weibo_snippet) VALUES ";
-        db.run(insert + "('Bill', 'http://', 'What is what', 'https://baidu.com/', 'What what. @what');");
-        db.run(insert + "('Yunhai Lu', 'http://', 'Mengmeng', 'http://baidu.com/', 'Ha hi ho.')");
-        db.run(insert + "('Bingo Du', 'http://', 'COO', 'http://baidu.com/', 'Ha hi ho.')");
-        db.run(insert + "('Zhenyu Li', 'http://', 'Disigner', 'http://baidu.com/', 'Ha hi ho.')");
-        db.run(insert + "('Peter Jiang', 'http://', 'CEO', 'http://baidu.com/', 'Ha hi ho.')");
-        db.run(insert + "('Jemmey Ji', 'http://', 'Tech', 'http://baidu.com/', 'Ha hi ho.')");
-        db.run(insert + "('Flora Zhu', 'http://', 'PM', 'http://baidu.com/', 'Ha hi ho.')");
-        db.run(insert + "('Hongfang Fan', 'http://', 'Purchasing', 'http://baidu.com/', 'Ha hi ho.')");
-        db.run(insert + "('Yunqiang Dai', 'http://', 'Struct', 'http://baidu.com/', 'Ha hi ho.')");
-        db.run(insert + "('Bruse Zheng', 'http://', 'Struct', 'http://baidu.com/', 'Ha hi ho.')");
-        db.run(insert + "('Nader', 'http://', 'Intern', 'http://baidu.com/', 'Ha hi ho.')");
-        db.run("UPDATE member SET image_url = 'http://tp3.sinaimg.cn/1852220282/50/5622385613/0'");
-      }
-      console.log("close db");
-      db.close()
+fs.mkdir("db", function() {
+  var db = new sqlite.Database('db/db.db', function() {
+    db.run("create table member(\
+      id integer PRIMARY KEY autoincrement\
+      , name varchar(15)\
+      , image_url varchar(1000)\
+      , intro varchar(100)\
+      , weibo_url varchar(100)\
+      , weibo_snippet varchar(1000)\
+      , position integer default 10\
+      , join_date integer\
+    )"
+      , function(error) {
+        if (!error) {
+          console.log("Insert test data");
+          var insert = "INSERT INTO member(name, image_url, intro, weibo_url, weibo_snippet) VALUES ";
+          db.run(insert + "('Bill', 'http://', 'What is what', 'https://baidu.com/', 'What what. @what');");
+          db.run(insert + "('Yunhai Lu', 'http://', 'Mengmeng', 'http://baidu.com/', 'Ha hi ho.')");
+          db.run(insert + "('Bingo Du', 'http://', 'COO', 'http://baidu.com/', 'Ha hi ho.')");
+          db.run(insert + "('Zhenyu Li', 'http://', 'Disigner', 'http://baidu.com/', 'Ha hi ho.')");
+          db.run(insert + "('Peter Jiang', 'http://', 'CEO', 'http://baidu.com/', 'Ha hi ho.')");
+          db.run(insert + "('Jemmey Ji', 'http://', 'Tech', 'http://baidu.com/', 'Ha hi ho.')");
+          db.run(insert + "('Flora Zhu', 'http://', 'PM', 'http://baidu.com/', 'Ha hi ho.')");
+          db.run(insert + "('Hongfang Fan', 'http://', 'Purchasing', 'http://baidu.com/', 'Ha hi ho.')");
+          db.run(insert + "('Yunqiang Dai', 'http://', 'Struct', 'http://baidu.com/', 'Ha hi ho.')");
+          db.run(insert + "('Bruse Zheng', 'http://', 'Struct', 'http://baidu.com/', 'Ha hi ho.')");
+          db.run(insert + "('Nader', 'http://', 'Intern', 'http://baidu.com/', 'Ha hi ho.')");
+          db.run("UPDATE member SET image_url = 'http://tp3.sinaimg.cn/1852220282/50/5622385613/0'");
+        } else {
+          console.log("Table exists");
+        }
+        console.log("Close db");
+        db.close()
+    })
   })
 })
-
 
 
 app.use('/', routes);
