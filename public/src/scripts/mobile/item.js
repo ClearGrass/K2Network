a4377505dcd8cfdd277aa5c94a98b2cf29a4f126
yunhailu <644897271@qq.com>
2016-05-31 22:57:53 +0800

    wx share

--- public/src/scripts/mobile/item.js
+++ public/src/scripts/mobile/item.js
@@ -15,10 +15,5 @@ $(function(){
         document.title = resp.name ? "K2 - " + resp.name : "K2 创业网络";
     });
 
-    wxBridge && wxBridge.weixinShareTimeline({
-        "img_url": "/images/logo@2x.png",
-        "link": "http://k2.cm",
-        "desc": "K2 创业网络",
-        "title": "K2 创业网络"
-    });
+    wxBridge.init();
 });
